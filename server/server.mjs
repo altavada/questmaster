5dfebaea6386a525ade95b59bb4be4162ef530c1
altavada <117209509+altavada@users.noreply.github.com>
2023-08-20 14:45:06 -0400

    server and web token

--- server/server.mjs
+++ server/server.mjs
@@ -1,17 +1,27 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs";
+import path from "path";
+import db from './db/conn.mjs'
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/record", records);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+// opens db and starts the Express server
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+})
