ba55f86ee2de75f5816715c6fc60e4d687d1aec1
altavada <117209509+altavada@users.noreply.github.com>
2023-09-11 16:43:19 -0400

    react config and app.js, header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="powered-by">
+        <p>Powered by</p>
+        <p id="brand-name">QuestMaster</p>
+        <div id="logo-plate">
+
+        </div>
+      </div>
+    </header>
+  );
+}
