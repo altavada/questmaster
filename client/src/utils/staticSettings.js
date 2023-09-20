fb58e43c4af3bface6f796dddf4e818e1342d91a
altavada <117209509+altavada@users.noreply.github.com>
2023-09-20 18:15:40 -0400

    code cleanup

---
+++ client/src/utils/staticSettings.js
@@ -0,0 +1,37 @@
+export const bookingPrefs = {
+  timeZone: "America/New_York",
+  bookingWindow: 7,
+  dailyHours: {
+    0: {
+      isOpen: false,
+    },
+    1: {
+      isOpen: true,
+      opens: "09:00",
+      closes: "17:00",
+    },
+    2: {
+      isOpen: true,
+      opens: "09:00",
+      closes: "17:00",
+    },
+    3: {
+      isOpen: true,
+      opens: "09:00",
+      closes: "17:00",
+    },
+    4: {
+      isOpen: true,
+      opens: "09:00",
+      closes: "17:00",
+    },
+    5: {
+      isOpen: true,
+      opens: "09:00",
+      closes: "17:00",
+    },
+    6: {
+      isOpen: false,
+    },
+  },
+};
