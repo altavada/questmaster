e7f1ce433a738d0e1d91e8e9f5b8e53cd1eac8a4
altavada <117209509+altavada@users.noreply.github.com>
2023-09-22 22:37:22 -0400

    code cleaning / error handling

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,28 +7,25 @@ module.exports = {
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
-
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
-
     if (!token) {
       return res.status(400).json({ message: "You have no token!" });
     }
-
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log("Invalid token");
-      return res.status(400).json({ message: "invalid token!" });
+      return res.status(400).json({ message: "Invalid token!" });
     }
-
     // send to next endpoint
     next();
   },
+
   signToken: ({ name, email, _id, isAdmin }) => {
     const payload = { name, email, _id, isAdmin };
 
