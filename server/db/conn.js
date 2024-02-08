fb4a779483b9239f50469e9483d236d3354f84dd
altavada <117209509+altavada@users.noreply.github.com>
2024-02-08 17:28:02 -0500

    heroku troubleshoot

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.ATLAS_URI);
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
